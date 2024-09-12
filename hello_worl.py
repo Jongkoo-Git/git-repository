d7384a663e7ad98dddedf4b75467d87a3ed82597
Jongkoo-git <kjk050511@gmail.com>
2024-09-12 17:34:55 +0900

    update

--- hello_worl.py
+++ hello_worl.py
@@ -1,5 +1,9 @@
 def print_hello():
     print("Hello World")
 
+def print_Kit():
+    print("kumoh National Institute of Technology.")
+
 if __name__ == '__main__':
+    print_Kit()
     print_hello()
